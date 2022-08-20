6a2e5f11c9fa35c66949bbf3492927a3c857be52
vsevolod19860507 <vsevolod19860507@icloud.com>
2022-08-20 09:42:04 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Vsevolod on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
